8991b6d54a36c92d9b4994901719a5c35c46861c
saaaaraaaah <stre757@aucklanduni.ac.nz>
2019-09-11 14:09:59 +1200

    "Main class started"

---
+++ Assignment3/src/application/Main.java
@@ -0,0 +1,28 @@
+package application;
+
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+
+public class Main extends Application {
+	
+	@Override
+	public void start(Stage primaryStage) {
+		try {
+			FXMLLoader loader = new FXMLLoader();
+			loader.setLocation(getClass().getResource("Menu.fxml"));
+			Parent layout = loader.load();
+			Scene scene = new Scene(layout);
+			primaryStage.setScene(scene);
+			primaryStage.show();
+		} catch(Exception e) {
+			e.printStackTrace();
+		}
+	}
+	
+	public static void main(String[] args) {
+		launch(args);
+	}
+}
