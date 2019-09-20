8cd9b40b8e2d17231bf1578261c80a68cfcf8e34
jshi213 <jshi213@aucklanduni.ac.nz>
2019-09-20 14:00:05 +1200

    preview finalization

--- Assignment3/src/application/CreateMenuController.java
+++ Assignment3/src/application/CreateMenuController.java
@@ -1,10 +1,18 @@
 package application;
 
+import java.io.IOException;
+import java.io.PrintWriter;
 import java.nio.file.Files;
 import java.nio.file.Paths;
 
+import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Node;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
 import javafx.scene.control.TextArea;
+import javafx.stage.Stage;
 
 public class CreateMenuController {
 	
@@ -24,5 +32,16 @@ public class CreateMenuController {
 		}
 	}
 	
-	
+	@FXML
+	private void handleButtonPreview(ActionEvent event) throws IOException {
+		String previewText = textAreaResults.getSelectedText();
+		System.out.println(previewText);
+		PrintWriter writer = new PrintWriter("previewtext", "UTF-8");
+		writer.println(previewText);
+		writer.close();
+		
+		ProcessBuilder pb1 = new ProcessBuilder();
+		pb1.command("bash", "-c", "text2wave previewtext -o temporary");
+		Process process2 = pb1.start();
+	}
 }
