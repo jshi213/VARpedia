94c57c70b0427ffe62b82a29350d118bc41249ed
jshi213 <jshi213@aucklanduni.ac.nz>
2019-09-29 17:30:45 +1300

    menuscene

---
+++ Assignment3/src/application/MenuScene.java
@@ -0,0 +1,25 @@
+package application;
+
+import java.io.IOException;
+
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+
+public class MenuScene implements Runnable {
+	public void run() {
+		Parent menuParent;
+		try {
+			menuParent = FXMLLoader.load(getClass().getResource("Menu.fxml"));
+			Scene menuScene =  new Scene(menuParent);
+			Stage stage = SearchController.getStage();
+			stage.setScene(menuScene);
+			stage.show();
+			ImageSelectorController.getAlert().close();
+		} catch (IOException e) {
+		e.printStackTrace();
+		}
+	}
+
+}
