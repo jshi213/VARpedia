bec4f8ae81eb8056d6b71d120bc872e702c40946
stre757 <stre757@en-368401.uoa.auckland.ac.nz>
2019-09-29 16:07:29 +1300

    video creation

--- Assignment3/src/application/FlickrProcess.java
+++ Assignment3/src/application/FlickrProcess.java
@@ -11,6 +11,10 @@ import java.io.PrintWriter;
 import java.io.Writer;
 
 import javax.imageio.ImageIO;
+import javax.sound.sampled.AudioFormat;
+import javax.sound.sampled.AudioInputStream;
+import javax.sound.sampled.AudioSystem;
+import javax.sound.sampled.UnsupportedAudioFileException;
 
 import com.flickr4java.flickr.Flickr;
 import com.flickr4java.flickr.FlickrException;
@@ -54,7 +58,6 @@ public class FlickrProcess extends Task<Void> {
 	        params.setText(query);
 	        
 	        PhotoList<Photo> results = photos.search(params, resultsPerPage, page);
-//	        System.out.println("Retrieving " + results.size()+ " results");
 	        
 	        for (Photo photo: results) {
 	        	try {
@@ -62,21 +65,19 @@ public class FlickrProcess extends Task<Void> {
 		        	String filename = query.trim().replace(' ', '-')+"-"+System.currentTimeMillis()+"-"+photo.getId()+".jpg";
 		        	File outputfile = new File("downloads",filename);
 		        	ImageIO.write(image, "jpg", outputfile);
-//		        	System.out.println("Downloaded "+filename);
 	        	} catch (FlickrException fe) {
-//	        		System.err.println("Ignoring image " +photo.getId() +": "+ fe.getMessage());
 				}
 	        }
 		} catch (Exception e) {
 			e.printStackTrace();
 		}
 		
-//		System.out.println("\nDone");
 		return null;
 	}
 	
 	@Override
 	protected void done() {
+		// after retrieving photos, create the video for the creation
 		File tempScript = tempScript();
 
 		try {
@@ -89,26 +90,49 @@ public class FlickrProcess extends Task<Void> {
 		} finally {
 			tempScript.delete();
 		}
+
 	}
 	
 	private File tempScript() {
+		// find the number of seconds to play each image for
+		double durationInSeconds = 0;
+		try {
+			File file = new File("audiofiles/combined.wav");
+			AudioInputStream audioInputStream;
+			audioInputStream = AudioSystem.getAudioInputStream(file);
+			AudioFormat format =audioInputStream.getFormat();
+			long frames = audioInputStream.getFrameLength();
+			durationInSeconds = (frames+0.0) / format.getFrameRate();
+		} catch (UnsupportedAudioFileException | IOException e1) {
+			// TODO Auto-generated catch block
+			e1.printStackTrace();
+		}
+		
+		double secondsPerImage = durationInSeconds / _number;
+		
+		// create temporary shell script to run commands for video creation
 		try {
 			File tempScript = File.createTempFile("script", null);
 			Writer streamWriter = new OutputStreamWriter(new FileOutputStream(tempScript));
 			PrintWriter printWriter = new PrintWriter(streamWriter);
 			printWriter.println("#!/bin/bash");
 			printWriter.println("cd downloads");
-			printWriter.println("ffmpeg -framerate 1 -pattern_type glob -i '*.jpg' -c:v libx264 -vf \"pad=ceil(iw/2)*2:ceil(ih/2)*2\" -r 30 -pix_fmt yuv420p slideshow.mp4");
-			printWriter.println("ffmpeg -stream_loop -1 -i slideshow.mp4 -i ../audiofiles/combined.wav -shortest -map 0:v:0 -map 1:a:0 -y audioslideshow.mp4");
+			// create slideshow of images
+			printWriter.println("ffmpeg -framerate 1/" + secondsPerImage + " -pattern_type glob -i '*.jpg' -c:v libx264 -vf \"pad=ceil(iw/2)*2:ceil(ih/2)*2\" -r 30 -pix_fmt yuv420p -t " + secondsPerImage*_number + " slideshow.mp4 ");
+			// combine slideshow and audio
+			printWriter.println("ffmpeg -i slideshow.mp4 -i ../audiofiles/combined.wav -shortest -map 0:v:0 -map 1:a:0 -y audioslideshow.mp4");
+			// add the search term to video
 			printWriter.println("ffmpeg -i audioslideshow.mp4 -vf \"drawtext=fontfile=myfont.ttf:fontsize=60: fontcolor=white:x=(w-text_w)/2:y=(h-text_h)/2:text=" + SearchController._searchTerm + "\" -codec:a copy ../Creations/" + _creation + ".mp4");
 			printWriter.println("rm -f slideshow.mp4 audioslideshow.mp4 *.jpg");
-			printWriter.println("rm -rf ../audiofiles ../temporaryfiles ../downloads");
+			printWriter.println("rm -rf  ../temporaryfiles ../downloads");
 			printWriter.close();
 			return tempScript;
 		} catch (IOException e) {
 			e.printStackTrace();
 		}
 		return null;
+		
+		
 	}
 	
 //	public static String getAPIKey(String key) throws Exception {
