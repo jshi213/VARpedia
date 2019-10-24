adea8a250a88c637882010cb064e9530f20141e4
stre757 <stre757@en-369731.uoa.auckland.ac.nz>
2019-10-24 16:03:34 +1300

    merged handle speeds into one method

--- Assignment3/src/application/controller/MenuController.java
+++ Assignment3/src/application/controller/MenuController.java
@@ -394,33 +394,12 @@ public class MenuController {
 	}
 	
 	@FXML
-	private void handleSpeedOne(ActionEvent event) {
-		_voiceRate = "(Parameter.set 'Duration_Stretch 2)\n";
-		buttonVoiceRate.setText("0.5x");
-	}
-	
-	@FXML
-	private void handleSpeedTwo(ActionEvent event) {
-		_voiceRate = "(Parameter.set 'Duration_Stretch 1.333)\n";
-		buttonVoiceRate.setText("0.75x");
-	}
-	
-	@FXML
-	private void handleSpeedThree(ActionEvent event) {
-		_voiceRate = "";
-		buttonVoiceRate.setText("1x");
-	}
-	
-	@FXML
-	private void handleSpeedFour(ActionEvent event) {
-		_voiceRate = "(Parameter.set 'Duration_Stretch 0.666)\n";
-		buttonVoiceRate.setText("1.5x");
-	}
-	
-	@FXML
-	private void handleSpeedFive(ActionEvent event) {
-		_voiceRate = "(Parameter.set 'Duration_Stretch 0.5\n)";
-		buttonVoiceRate.setText("2x");
+	private void handleSpeed(ActionEvent event) {
+		String speedItem = ((MenuItem) event.getSource()).getText();
+		String speed = speedItem.substring(0, speedItem.length()-1);
+		double speedNum = 1/Double.valueOf(speed);
+		_voiceRate = "(Parameter.set 'Duration_Stretch " + speedNum + ")\n";
+		buttonVoiceRate.setText(speedItem);
 	}
 	
 	@FXML
